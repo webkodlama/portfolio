c7489ef97c0a9cdf0c76fbd5b72879d84ce78676
webkodlama <153864977+webkodlama@users.noreply.github.com>
2026-04-28 00:21:39 +0300

    Add files via upload

---
+++ assets/js/english.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+
+    // Add Active to Sidebar link when click
+    $(".sidebar nav li a").click(function () {
+        $(".sidebar nav li a").removeClass('active');
+        $(this).addClass('active');
+    });
+
+    // Click on Get Started to scroll down
+    $('.homepage .hero-section .get-started').click(function () {
+        $('html,body').animate({
+            scrollTop: $(".homepage .hero-section").innerHeight()
+        }, '100')
+    });
+
+    $('.sidebar-toggle').click(function(){
+        $(this).toggleClass('active');
+        $('.homepage .sidebar').toggleClass('active')
+    })
+
+    // Initiate Typed js - English
+    new Typed('#typed', {
+        strings: ['Front-End Developer', 'UI Designer', 'JavaScript Enthusiast'],
+        typeSpeed: 60,
+        backSpeed: 40,
+        backDelay: 1500,
+        loop: true
+    });
+
+    // Init Image Filter
+    var $grid = $('.grid').isotope({
+        itemSelector: '.element-item',
+        layoutMode: 'fitRows'
+    });
+
+    var filterFns = {
+        numberGreaterThan50: function () {
+            var number = $(this).find('.number').text();
+            return parseInt(number, 10) > 50;
+        },
+        ium: function () {
+            var name = $(this).find('.name').text();
+            return name.match(/ium$/);
+        }
+    };
+
+    // filter items on button click
+    $('.filter-button-group').on('click', 'button', function () {
+        $(this).addClass('active').siblings().removeClass('active')
+        var filterValue = $(this).attr('data-filter');
+        filterValue = filterFns[filterValue] || filterValue;
+        $grid.isotope({ filter: filterValue });
+    });
+
+});
+
+$(window).on('load',function(){
+    $(".loading").addClass('endLoading').fadeOut(2000)
+});
